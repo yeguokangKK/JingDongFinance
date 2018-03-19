61e368ef2c7e17371f10370eebf8bc172f6b664a
JtoJ <34855132+JtoJ@users.noreply.github.com>
2018-03-19 18:02:34 +0800

    增加核心组件和公用组件

---
+++ app/js/core/slider.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+  <section :class="cname">
+    <swiper :options="options" :not-next-tick="options.notNextTick">
+      <swiper-slide v-for="item in items" :key="item.href">
+        <router-link :to="{ name: item.href}">
+          <img :src="item.src" alt="">
+        </router-link>
+      </swiper-slide>
+      <div class="swiper-pagination" v-if="options.pagination"/>
+    </swiper>
+  </section>
+</template>
+
+<script>
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
+export default {
+  components:{
+    swiper,
+    swiperSlide
+  },
+  props:{
+    options:{
+      type:Object,
+      default(){
+        return{
+          autoplay:true,
+          loop:true,
+          pagination:{
+            el:'.swiper-pagination',
+          },
+          notNextTick:false,
+        }
+      }
+    },
+    items:{
+      type:Array,
+      default(){
+        //格式{href:'',src:''},跳转的链接和图片的地址
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  @import "~swiper/dist/css/swiper.css"
+</style>
